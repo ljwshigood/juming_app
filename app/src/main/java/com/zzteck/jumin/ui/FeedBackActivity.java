16994759a54ac29a32caebcb973fe9f953b9a30f
Hupo <ljwshigood@163.com>
2019-01-11 20:24:48 +0800

    fadfa

---
+++ app/src/main/java/com/zzteck/jumin/ui/FeedBackActivity.java
@@ -0,0 +1,67 @@
+package com.zzteck.jumin.ui;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+
+import com.hjhrq1991.library.tbs.BridgeHandler;
+import com.hjhrq1991.library.tbs.CallBackFunction;
+import com.hjhrq1991.library.tbs.DefaultHandler;
+import com.hjhrq1991.library.tbs.SimpleBridgeWebViewClientListener;
+import com.hjhrq1991.library.tbs.TbsBridgeWebView;
+import com.tencent.smtt.export.external.interfaces.SslError;
+import com.tencent.smtt.export.external.interfaces.SslErrorHandler;
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.zzteck.jumin.R;
+import com.zzteck.jumin.app.App;
+
+
+public class FeedBackActivity extends BaseActivity implements OnClickListener{
+
+
+	private void initView() {
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_feek_back);
+
+
+ 		initView() ;
+	}
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		}
+	}
+
+}
