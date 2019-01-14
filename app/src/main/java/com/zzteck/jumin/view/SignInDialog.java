0b332027c1392c6586a7dceeea6cde1bbf8517c2
Hupo <ljwshigood@163.com>
2019-01-14 20:27:17 +0800

    fasfsdafssd

---
+++ app/src/main/java/com/zzteck/jumin/view/SignInDialog.java
@@ -0,0 +1,54 @@
+package com.zzteck.jumin.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.zzteck.jumin.R;
+import com.zzteck.jumin.ui.BaseActivity;
+
+
+public class SignInDialog extends BaseActivity implements OnClickListener{
+
+
+	private void initView() {
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_feek_back);
+
+
+ 		initView() ;
+	}
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		}
+	}
+
+}
