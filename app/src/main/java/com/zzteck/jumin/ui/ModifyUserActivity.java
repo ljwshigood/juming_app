78b15b3a9a66c19a368e4d6f142ab750c982734f
Hupo <ljwshigood@163.com>
2019-01-14 20:25:45 +0800

    fasfsdafssd

---
+++ app/src/main/java/com/zzteck/jumin/ui/ModifyUserActivity.java
@@ -0,0 +1,75 @@
+package com.zzteck.jumin.ui;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+
+import com.hjhrq1991.library.tbs.BridgeHandler;
+import com.hjhrq1991.library.tbs.CallBackFunction;
+import com.hjhrq1991.library.tbs.DefaultHandler;
+import com.hjhrq1991.library.tbs.SimpleBridgeWebViewClientListener;
+import com.hjhrq1991.library.tbs.TbsBridgeWebView;
+import com.tencent.smtt.export.external.interfaces.SslError;
+import com.tencent.smtt.export.external.interfaces.SslErrorHandler;
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.zzteck.jumin.R;
+import com.zzteck.jumin.app.App;
+
+
+public class ModifyUserActivity extends BaseActivity implements OnClickListener{
+
+	TbsBridgeWebView webView;
+
+	private String TAG = "liujw" ;
+
+	private void initView() {
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_modify_user);
+
+		mContext = ModifyUserActivity.this ;
+		App.getInstance().addActivity(this);
+
+ 		initView() ;
+	}
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		}
+	}
+
+	@Override
+	public void onBackPressed() {
+
+	}
+}
