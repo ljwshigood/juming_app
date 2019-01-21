d15090d584052ddb6483611975bda40277112145
Hupo <ljwshigood@163.com>
2019-01-21 11:21:31 +0800

    fads

--- app/src/main/java/com/zzteck/jumin/fragment/HomeFragment.java
+++ app/src/main/java/com/zzteck/jumin/fragment/HomeFragment.java
@@ -3,28 +3,51 @@ package com.zzteck.jumin.fragment;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Color;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.ColorUtils;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
 
 import com.github.jdsjlzx.recyclerview.LRecyclerView;
 import com.github.jdsjlzx.recyclerview.LRecyclerViewAdapter;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.listener.SimpleMultiPurposeListener;
+import com.scwang.smartrefresh.layout.util.DensityUtil;
 import com.youth.banner.Banner;
 import com.zzteck.jumin.R;
+import com.zzteck.jumin.adapter.ComFragmentAdapter;
 import com.zzteck.jumin.adapter.HomeAdapter;
 import com.zzteck.jumin.bean.HomeBean;
+import com.zzteck.jumin.view.ColorFlipPagerTitleView;
+import com.zzteck.jumin.view.JudgeNestedScrollView;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.UIUtil;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -34,12 +57,43 @@ import java.util.List;
  */
 
 public class HomeFragment extends BaseFragment {
-    private LinearLayout mToolbar;
-    private LRecyclerView mRecyclerView;
+
     private List<HomeBean> data;
     private int height;
 
-    private LRecyclerViewAdapter mLRecyclerViewAdapter = null;
+    private SmartRefreshLayout refreshLayout ;
+
+    private JudgeNestedScrollView scrollView;
+
+    private MagicIndicator magicIndicator;
+
+    private MagicIndicator magicIndicatorTitle;
+
+    private String[] mTitles = new String[]{"推荐", "二手房", "商家"};
+
+    private List<String> mDataList = Arrays.asList(mTitles);
+
+    private RecommandFragment mRecomandFragment ;
+
+    private SecondHandFragment mSecondHandFragment ;
+
+    private BusinessFragment mBusinessFragment ;
+
+    private ViewPager viewPager;
+
+    private List<Fragment> getFragments() {
+        List<Fragment> fragments = new ArrayList<>();
+
+        mRecomandFragment = new RecommandFragment() ;
+        mSecondHandFragment = new SecondHandFragment() ;
+        mBusinessFragment = new BusinessFragment() ;
+
+        fragments.add(mRecomandFragment);
+        fragments.add(mSecondHandFragment);
+        fragments.add(mBusinessFragment);
+
+        return fragments;
+    }
 
     @Override
     public int getLayoutId() {
@@ -48,120 +102,164 @@ public class HomeFragment extends BaseFragment {
 
 
     @Override
-    public void initView(View mView) {
-        mToolbar = mView.findViewById(R.id.toolbar);
-        mRecyclerView = mView.findViewById(R.id.rv_home);
+    public void initView(View view) {
+
+        viewPager = view.findViewById(R.id.view_pager) ;
+        refreshLayout = view.findViewById(R.id.refreshLayout) ;
+        scrollView = view.findViewById(R.id.scrollView) ;
+        magicIndicator = view.findViewById(R.id.magic_indicator) ;
+        magicIndicatorTitle = view.findViewById(R.id.magic_indicator_title) ;
+        refreshLayout.setOnMultiPurposeListener(new SimpleMultiPurposeListener() {
+            @Override
+            public void onHeaderPulling(RefreshHeader header, float percent, int offset, int bottomHeight, int extendHeight) {
+               /* mOffset = offset / 2;
+                ivHeader.setTranslationY(mOffset - mScrollY);*/
+            }
+
+            @Override
+            public void onHeaderReleasing(RefreshHeader header, float percent, int offset, int bottomHeight, int extendHeight) {
+               /* mOffset = offset / 2;
+                ivHeader.setTranslationY(mOffset - mScrollY);*/
+            }
+        });
+
+        scrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            int lastScrollY = 0;
+            int h = DensityUtil.dp2px(170);
+            int color = ContextCompat.getColor(getActivity(), R.color.mainWhite) & 0x00ffffff;
+
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                int[] location = new int[2];
+                magicIndicator.getLocationOnScreen(location);
+                int yPosition = location[1];
+               /* if (yPosition < toolBarPositionY) {
+                    magicIndicatorTitle.setVisibility(View.VISIBLE);
+                    scrollView.setNeedScroll(false);
+                } else {
+                    magicIndicatorTitle.setVisibility(View.GONE);
+                    scrollView.setNeedScroll(true);
+
+                }*/
+
+                if (lastScrollY < h) {
+                    scrollY = Math.min(h, scrollY);
+                  /*  mScrollY = scrollY > h ? h : scrollY;
+                    buttonBarLayout.setAlpha(1f * mScrollY / h);
+                    toolbar.setBackgroundColor(((255 * mScrollY / h) << 24) | color);
+                    ivHeader.setTranslationY(mOffset - mScrollY);*/
+                }
+                /*if (scrollY == 0) {
+                    ivBack.setImageResource(R.drawable.back_white);
+                    ivMenu.setImageResource(R.drawable.icon_menu_white);
+                } else {
+                    ivBack.setImageResource(R.drawable.back_black);
+                    ivMenu.setImageResource(R.drawable.icon_menu_black);
+                }*/
+
+                lastScrollY = scrollY;
+            }
+        });
+
+        viewPager.setAdapter(new ComFragmentAdapter(getActivity().getSupportFragmentManager(), getFragments()));
+        viewPager.setOffscreenPageLimit(10);
+        initMagicIndicator();
+        initMagicIndicatorTitle();
+
     }
 
-    private List<HomeBean> mHomeList = new ArrayList<>() ;
+    private void initMagicIndicator() {
+        CommonNavigator commonNavigator = new CommonNavigator(getActivity());
+        commonNavigator.setScrollPivotX(0.65f);
+        commonNavigator.setAdjustMode(true);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+            @Override
+            public int getCount() {
+                return mDataList == null ? 0 : mDataList.size();
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                SimplePagerTitleView simplePagerTitleView = new ColorFlipPagerTitleView(context);
+                simplePagerTitleView.setText(mDataList.get(index));
+                simplePagerTitleView.setNormalColor(ContextCompat.getColor(getActivity(), R.color.mainBlack));
+                simplePagerTitleView.setSelectedColor(ContextCompat.getColor(getActivity(), R.color.mainBlack));
+                simplePagerTitleView.setTextSize(16);
+                simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        viewPager.setCurrentItem(index, false);
+                    }
+                });
+                return simplePagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator indicator = new LinePagerIndicator(context);
+                indicator.setMode(LinePagerIndicator.MODE_EXACTLY);
+                indicator.setLineHeight(UIUtil.dip2px(context, 2));
+                indicator.setLineWidth(UIUtil.dip2px(context, 20));
+                indicator.setRoundRadius(UIUtil.dip2px(context, 3));
+                indicator.setStartInterpolator(new AccelerateInterpolator());
+                indicator.setEndInterpolator(new DecelerateInterpolator(2.0f));
+                indicator.setColors(ContextCompat.getColor(getActivity(), R.color.mainRed));
+                return indicator;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(magicIndicator, viewPager);
+    }
+
+    private void initMagicIndicatorTitle() {
+        CommonNavigator commonNavigator = new CommonNavigator(getActivity());
+        commonNavigator.setScrollPivotX(0.65f);
+        commonNavigator.setAdjustMode(true);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+            @Override
+            public int getCount() {
+                return mDataList == null ? 0 : mDataList.size();
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                SimplePagerTitleView simplePagerTitleView = new ColorFlipPagerTitleView(context);
+                simplePagerTitleView.setText(mDataList.get(index));
+                simplePagerTitleView.setNormalColor(ContextCompat.getColor(getActivity(), R.color.mainBlack));
+                simplePagerTitleView.setSelectedColor(ContextCompat.getColor(getActivity(), R.color.mainBlack));
+                simplePagerTitleView.setTextSize(16);
+                simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        viewPager.setCurrentItem(index, false);
+                    }
+                });
+                return simplePagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator indicator = new LinePagerIndicator(context);
+                indicator.setMode(LinePagerIndicator.MODE_EXACTLY);
+                indicator.setLineHeight(UIUtil.dip2px(context, 2));
+                indicator.setLineWidth(UIUtil.dip2px(context, 20));
+                indicator.setRoundRadius(UIUtil.dip2px(context, 3));
+                indicator.setStartInterpolator(new AccelerateInterpolator());
+                indicator.setEndInterpolator(new DecelerateInterpolator(2.0f));
+                indicator.setColors(ContextCompat.getColor(getActivity(), R.color.mainRed));
+                return indicator;
+            }
+        });
+        magicIndicatorTitle.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(magicIndicatorTitle, viewPager);
+
+    }
 
     @Override
     public void lazyLoad() {
 
-        LinearLayoutManager gridLayoutManager = new LinearLayoutManager(getContext());
-        mRecyclerView.setLayoutManager(gridLayoutManager);
 
-        String jsonData = new String(getAssertsFile(getContext(), "content.json"));
-
-        data = new Gson().fromJson(jsonData, new TypeToken<List<HomeBean>>() {}.getType());
-
-        for(int i = 0 ;i < 3;i++){
-            HomeBean bean = new HomeBean() ;
-            bean.setType(i);
-            mHomeList.add(bean) ;
-        }
-
-        HomeAdapter adapter = new HomeAdapter(getActivity(),mHomeList,getFragmentManager());
-
-
-        mLRecyclerViewAdapter = new LRecyclerViewAdapter(adapter) ;
-
-        mRecyclerView.setAdapter(mLRecyclerViewAdapter);
-
-        mLRecyclerViewAdapter.addHeaderView(getHeaderView(mRecyclerView));
-
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            private int totalDy = 0;
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                totalDy += dy;
-                if (totalDy <= height) {
-                    float alpha = (float) totalDy / height;
-                    mToolbar.setBackgroundColor(ColorUtils.blendARGB(Color.TRANSPARENT
-                            , ContextCompat.getColor(getContext(), R.color.white), alpha));
-                } else {
-                    mToolbar.setBackgroundColor(ColorUtils.blendARGB(Color.TRANSPARENT
-                            , ContextCompat.getColor(getContext(), R.color.white), 1));
-                }
-            }
-        });
-
-        mRecyclerView.setNestedScrollingEnabled(false);
     }
 
-    private View getHeaderView(RecyclerView v) {
-        List<String> bannerImg = new ArrayList<>();
-        bannerImg.add("https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1546918492&di=062431ef3056895f59346be28583583f&src=http://b.hiphotos.baidu.com/zhidao/pic/item/14ce36d3d539b600d3924a1feb50352ac65cb73e.jpg");
-        bannerImg.add("https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1546918492&di=062431ef3056895f59346be28583583f&src=http://b.hiphotos.baidu.com/zhidao/pic/item/14ce36d3d539b600d3924a1feb50352ac65cb73e.jpg");
-        bannerImg.add("https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1546918492&di=062431ef3056895f59346be28583583f&src=http://b.hiphotos.baidu.com/zhidao/pic/item/14ce36d3d539b600d3924a1feb50352ac65cb73e.jpg");
-        bannerImg.add("https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1546918492&di=062431ef3056895f59346be28583583f&src=http://b.hiphotos.baidu.com/zhidao/pic/item/14ce36d3d539b600d3924a1feb50352ac65cb73e.jpg");
-
-        View convertView = LayoutInflater
-                .from(getContext())
-                .inflate(R.layout.app_include_home_header, (ViewGroup) v.getParent(), false);
-
-        Banner mBanner = convertView.findViewById(R.id.app_home_header_banner);
-
-        mBanner.setImages(bannerImg)
-                .setDelayTime(3000)
-                .start();
-
-        ViewGroup.LayoutParams bannerParams = mBanner.getLayoutParams();
-        int resourceId = getContext().getResources().getIdentifier("status_bar_height", "dimen", "android");
-        int statusBarHeight = getContext().getResources().getDimensionPixelSize(resourceId);
-        height = bannerParams.height - statusBarHeight - 104;
-
-        return convertView;
-    }
-
-
-    public static byte[] getAssertsFile(Context context, String fileName) {
-        InputStream inputStream = null;
-        AssetManager assetManager = context.getAssets();
-        try {
-            inputStream = assetManager.open(fileName);
-            if (inputStream == null) {
-                return null;
-            }
-
-            BufferedInputStream bis = null;
-            int length;
-            try {
-                bis = new BufferedInputStream(inputStream);
-                length = bis.available();
-                byte[] data = new byte[length];
-                bis.read(data);
-
-                return data;
-            } catch (IOException e) {
-
-            } finally {
-                if (bis != null) {
-                    try {
-                        bis.close();
-                    } catch (Exception e) {
-
-                    }
-                }
-            }
-
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
 
 }
