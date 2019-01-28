54d8f5388bf194b0e50638998e8ce0759763e0dd
Hupo <ljwshigood@163.com>
2019-01-28 16:37:19 +0800

    fasfdas

--- app/src/main/java/com/zzteck/jumin/fragment/WJConversationListFragment.java
+++ app/src/main/java/com/zzteck/jumin/fragment/WJConversationListFragment.java
@@ -3,6 +3,8 @@ package com.zzteck.jumin.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.zzteck.jumin.R;
+import com.zzteck.jumin.adapter.HistoryAdapter;
+import com.zzteck.jumin.adapter.MessageAdapter;
+import com.zzteck.jumin.view.NormalDecoration;
 
 
 public class WJConversationListFragment extends Fragment {
@@ -21,6 +26,16 @@ public class WJConversationListFragment extends Fragment {
 
 	private RecyclerView mRVMessage ;
 
+	private MessageAdapter mMessageAdapter ;
+
+	private void initData(){
+
+		mRVMessage.setLayoutManager(new LinearLayoutManager(getActivity()));
+		mRVMessage.addItemDecoration(new NormalDecoration(ContextCompat.getColor(getActivity(), R.color.mainGrayF8), (int) getActivity().getResources().getDimension(R.dimen.one)));
+		mMessageAdapter = new MessageAdapter(getActivity(),null) ;
+		mRVMessage.setAdapter(mMessageAdapter) ;
+
+	}
 
 
 	@Override
@@ -29,6 +44,10 @@ public class WJConversationListFragment extends Fragment {
 		View convertionList = inflater.inflate(R.layout.conversation_list,container, false);
 		mTvMainInfo = convertionList.findViewById(R.id.tv_main_info) ;
 		mTvMainInfo.setText("消息");
+
+		mRVMessage = convertionList.findViewById(R.id.rv_message) ;
+
+		initData() ;
 		return convertionList;
 	}
 	
