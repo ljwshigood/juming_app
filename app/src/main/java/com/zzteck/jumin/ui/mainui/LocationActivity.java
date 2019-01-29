99f6d83de6d29c7d1a3e63f7640368682cd753fc
Hupo <ljwshigood@163.com>
2019-01-29 14:14:25 +0800

    fafd

---
+++ app/src/main/java/com/zzteck/jumin/ui/mainui/LocationActivity.java
@@ -0,0 +1,207 @@
+package com.zzteck.jumin.ui.mainui;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationProtocol;
+import com.amap.api.location.AMapLocationListener;
+import com.zzteck.jumin.R;
+
+/**
+ * 高精度定位模式功能演示
+ *
+ */
+public class LocationActivity extends BaseActivity  implements OnClickListener{
+
+	private TextView tvResult;
+	private Button btLocation;
+
+	private AMapLocationClient locationClient = null;
+	private AMapLocationClientOption locationOption = new AMapLocationClientOption();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_location);
+		initView();
+		initLocation();
+
+		locationOption.setLocationMode(AMapLocationMode.Hight_Accuracy);
+		startLocation();
+	}
+	
+	//初始化控件
+	private void initView(){
+
+		tvResult = (TextView) findViewById(R.id.tv_result);
+		btLocation = (Button) findViewById(R.id.bt_location);
+		
+		btLocation.setOnClickListener(this);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		destroyLocation();
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+
+		}
+	}
+
+
+	/**
+	 * 初始化定位
+	 * 
+	 * @since 2.8.0
+	 * @author hongming.wang
+	 *
+	 */
+	private void initLocation(){
+		//初始化client
+		locationClient = new AMapLocationClient(this.getApplicationContext());
+		//设置定位参数
+		locationClient.setLocationOption(getDefaultOption());
+		// 设置定位监听
+		locationClient.setLocationListener(locationListener);
+	}
+	
+	/**
+	 * 默认的定位参数
+	 * @since 2.8.0
+	 * @author hongming.wang
+	 *
+	 */
+	private AMapLocationClientOption getDefaultOption(){
+		AMapLocationClientOption mOption = new AMapLocationClientOption();
+		mOption.setLocationMode(AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
+		mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+		mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
+		mOption.setInterval(2000);//可选，设置定位间隔。默认为2秒
+		mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
+		mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
+		mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+		AMapLocationClientOption.setLocationProtocol(AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
+		mOption.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
+		mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+		mOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
+		return mOption;
+	}
+	
+	/**
+	 * 定位监听
+	 */
+	AMapLocationListener locationListener = new AMapLocationListener() {
+		@Override
+		public void onLocationChanged(AMapLocation loc) {
+			if (null != loc) {
+				//解析定位结果
+				//String result = Utils.getLocationStr(loc);
+				//tvResult.setText(result);
+			} else {
+				tvResult.setText("定位失败，loc is null");
+			}
+		}
+	};
+	
+	// 根据控件的选择，重新设置定位参数
+	private void resetOption() {
+		// 设置是否需要显示地址信息
+		locationOption.setNeedAddress(true);
+		/**
+		 * 设置是否优先返回GPS定位结果，如果30秒内GPS没有返回定位结果则进行网络定位
+		 * 注意：只有在高精度模式下的单次定位有效，其他方式无效
+		 */
+		locationOption.setGpsFirst(false);
+		// 设置是否开启缓存
+		locationOption.setLocationCacheEnable(false);
+		// 设置是否单次定位
+		locationOption.setOnceLocation(false);
+		//设置是否等待设备wifi刷新，如果设置为true,会自动变为单次定位，持续定位时不要使用
+		locationOption.setOnceLocationLatest(false);
+		//设置是否使用传感器
+		locationOption.setSensorEnable(false);
+		//设置是否开启wifi扫描，如果设置为false时同时会停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+		String strInterval = 2000+"";
+		if (!TextUtils.isEmpty(strInterval)) {
+			try{
+				// 设置发送定位请求的时间间隔,最小值为1000，如果小于1000，按照1000算
+				locationOption.setInterval(Long.valueOf(strInterval));
+			}catch(Throwable e){
+				e.printStackTrace();
+			}
+		}
+		
+		String strTimeout = 30000+"";
+		if(!TextUtils.isEmpty(strTimeout)){
+			try{
+				// 设置网络请求超时时间
+			     locationOption.setHttpTimeOut(Long.valueOf(strTimeout));
+			}catch(Throwable e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 开始定位
+	 * 
+	 * @since 2.8.0
+	 * @author hongming.wang
+	 *
+	 */
+	private void startLocation(){
+		//根据控件的选择，重新设置定位参数
+		resetOption();
+		// 设置定位参数
+		locationClient.setLocationOption(locationOption);
+		// 启动定位
+		locationClient.startLocation();
+	}
+
+	/**
+	 * 停止定位
+	 *
+	 * @since 2.8.0
+	 * @author hongming.wang
+	 *
+	 */
+	private void stopLocation(){
+		// 停止定位
+		locationClient.stopLocation();
+	}
+
+	/**
+	 * 销毁定位
+	 * 
+	 * @since 2.8.0
+	 * @author hongming.wang
+	 *
+	 */
+	private void destroyLocation(){
+		if (null != locationClient) {
+			/**
+			 * 如果AMapLocationClient是在当前Activity实例化的，
+			 * 在Activity的onDestroy中一定要执行AMapLocationClient的onDestroy
+			 */
+			locationClient.onDestroy();
+			locationClient = null;
+			locationOption = null;
+		}
+	}
+}
