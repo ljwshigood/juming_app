dbb5329daa2e0115f712462076a5373afa46a802
Hupo <ljwshigood@163.com>
2019-03-11 20:19:31 +0800

    fasfsdafssd

--- app/src/main/java/com/zzteck/jumin/ui/mainui/FeedBackActivity.java
+++ app/src/main/java/com/zzteck/jumin/ui/mainui/FeedBackActivity.java
@@ -6,6 +6,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.zzteck.jumin.R;
 import com.zzteck.jumin.ui.mainui.BaseActivity;
@@ -14,8 +18,25 @@ import com.zzteck.jumin.ui.mainui.BaseActivity;
 public class FeedBackActivity extends BaseActivity implements OnClickListener{
 
 
-	private void initView() {
+	private TextView mTvMainInfo ;
 
+	private ImageView mIvBack ;
+
+	private RelativeLayout mLLBack ;
+
+	private LinearLayout mLLCharge ;
+
+	private void initData(){
+		mTvMainInfo.setText("用户反馈");
+	}
+
+	private void initView() {
+		mLLBack = findViewById(R.id.ll_back) ;
+		mTvMainInfo = findViewById(R.id.tv_main_info) ;
+		mIvBack = findViewById(R.id.iv_back) ;
+		mIvBack.setOnClickListener(this);
+		mLLBack.setVisibility(View.VISIBLE);
+		mLLBack.setOnClickListener(this);
 	}
 
 	@Override
