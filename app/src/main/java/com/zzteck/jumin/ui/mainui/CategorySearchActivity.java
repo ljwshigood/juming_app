f4b84916dc12b2f900c77c8b395beb532963edfb
Hupo <ljwshigood@163.com>
2019-03-15 18:06:12 +0800

    fasfsdafssd

---
+++ app/src/main/java/com/zzteck/jumin/ui/mainui/CategorySearchActivity.java
@@ -0,0 +1,46 @@
+package com.zzteck.jumin.ui.mainui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.zzteck.jumin.R;
+import com.zzteck.jumin.adapter.SearchAdapter;
+import com.zzteck.jumin.bean.SearchBean;
+import com.zzteck.zzview.WJSpandGridView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategorySearchActivity extends BaseActivity implements View.OnClickListener{
+
+    private LinearLayout mLLBack ;
+
+    private List<SearchBean> mSearchList = new ArrayList<>();
+
+    private void initView(){
+        mLLBack = findViewById(R.id.ll_back) ;
+        mLLBack.setOnClickListener(this);
+    }
+
+    private void initData(){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView(R.layout.activity_category_search);
+        initView() ;
+        initData();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ll_back :
+                finish();
+                break ;
+        }
+    }
+}
