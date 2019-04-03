4c0090a338b4bfc42791b436fd7f1bab18c73e6e
Hupo <ljwshigood@163.com>
2019-04-03 14:11:54 +0800

    fdas

---
+++ app/src/main/java/com/zzteck/jumin/adapter/ImageAdapter.java
@@ -0,0 +1,105 @@
+package com.zzteck.jumin.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.zzteck.jumin.R;
+import com.zzteck.jumin.bean.MediaInfo;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/6/30.
+ * Describe:
+ */
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+
+    private Context mContext;
+
+    private List<MediaInfo> mMediaInfoList;
+
+    public ImageAdapterLister getmImageAdapterListener() {
+        return mImageAdapterListener;
+    }
+
+    public void setmImageAdapterListener(ImageAdapterLister mImageAdapterListener) {
+        this.mImageAdapterListener = mImageAdapterListener;
+    }
+
+    private ImageAdapterLister mImageAdapterListener ;
+
+    public interface  ImageAdapterLister{
+
+        public void imageAddListener();
+
+    }
+
+    public ImageAdapter(Context context, List<MediaInfo> list) {
+        this.mContext = context;
+        this.mMediaInfoList = list;
+    }
+
+    public void notifyImageListChange(List<MediaInfo> list){
+        this.mMediaInfoList = list ;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ImageAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_image, parent, false);
+        return new ImageAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ImageAdapter.ViewHolder holder, final int position) {
+
+        if(position == mMediaInfoList.size()){
+            holder.mIvPicture.setImageResource(R.mipmap.btn_tianjiatupian);
+        }else {
+
+            Glide.with(mContext)
+                    .load(mMediaInfoList.get(position).getFilePath())
+                    .placeholder(R.mipmap.ic_launcher)
+                    .error(R.mipmap.ic_launcher)
+                    .crossFade(300)
+                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    .into(holder.mIvPicture);
+        }
+
+        holder.mIvPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(position == mMediaInfoList.size()){
+                    if(mImageAdapterListener != null){
+                        mImageAdapterListener.imageAddListener();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMediaInfoList == null ? 0 : mMediaInfoList.size() + 1;
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView mIvPicture;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mIvPicture = itemView.findViewById(R.id.iv_picture) ;
+        }
+    }
+
+
+}
--- app/src/main/java/com/zzteck/jumin/ui/mainui/FeedBackActivity.java
+++ app/src/main/java/com/zzteck/jumin/ui/mainui/FeedBackActivity.java
@@ -3,6 +3,8 @@ package com.zzteck.jumin.ui.mainui;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -20,6 +22,7 @@ import com.baijiahulian.common.crop.ThemeConfig;
 import com.baijiahulian.common.crop.model.PhotoInfo;
 import com.zzteck.jumin.R;
 import com.zzteck.jumin.adapter.FeedAdapter;
+import com.zzteck.jumin.adapter.ImageAdapter;
 import com.zzteck.jumin.bean.FeedBack;
 import com.zzteck.jumin.bean.MediaInfo;
 import com.zzteck.jumin.ui.mainui.BaseActivity;
@@ -46,7 +49,14 @@ public class FeedBackActivity extends BaseActivity implements OnClickListener{
 
 	private ImageView mIvAddPicture ;
 
+	private RecyclerView mRvAddPicture ;
+
+	private ImageAdapter mImageAdapter ;
+
+	private List<PhotoInfo> mPhotoList = new ArrayList<>();
+
 	private void initData(){
+		mImageAdapter = new ImageAdapter(mContext,null) ;
 		List<FeedBack> list = new ArrayList<>() ;
 		for(int i = 0 ;i < mString.length ;i++){
 			FeedBack bean = new FeedBack();
@@ -62,9 +72,38 @@ public class FeedBackActivity extends BaseActivity implements OnClickListener{
 		mRVFeedBack.setAdapter(mFeedBackApdater);
 
 		mTvMainInfo.setText("用户反馈");
+
+		GridLayoutManager linearLayoutManager1 = new GridLayoutManager(this,4);
+		mRvAddPicture.setLayoutManager(linearLayoutManager1);
+		mRvAddPicture.setAdapter(mImageAdapter);
+
+		mImageAdapter.setmImageAdapterListener(new ImageAdapter.ImageAdapterLister() {
+			@Override
+			public void imageAddListener() {
+
+
+				BJCommonImageCropHelper.openImageMulti(FeedBackActivity.this, mPhotoList,4,
+						new ThemeConfig.Builder().setMainElementsColor(Color.parseColor("#00ccff")).setTitlebarRightButtonText(R.string.complete).build(), new BJCommonImageCropHelper.OnHandlerResultCallback(){
+
+							@Override
+							public void onHandlerSuccess(List<PhotoInfo> resultList) {
+
+								mPhotoList = resultList ;
+								mHandler.sendEmptyMessage(0) ;
+							}
+
+							@Override
+							public void onHandlerFailure(String errorMsg) {
+
+							}
+
+						});
+			}
+		});
 	}
 
 	private void initView() {
+		mRvAddPicture = findViewById(R.id.rv_add_pic) ;
 		mIvAddPicture = findViewById(R.id.iv_add_pic) ;
 		mRVFeedBack = findViewById(R.id.rl_feekback) ;
 		mLLCommit = findViewById(R.id.ll_commit) ;
@@ -74,6 +113,10 @@ public class FeedBackActivity extends BaseActivity implements OnClickListener{
 		mLLBack.setOnClickListener(this);
 		mLLCommit.setOnClickListener(this);
 		mIvAddPicture.setOnClickListener(this);
+	}
+
+	private void initMeidaList(List<MediaInfo> infoList){
+		mImageAdapter.notifyImageListChange(infoList);
 	}
 
 	@Override
@@ -101,6 +144,31 @@ public class FeedBackActivity extends BaseActivity implements OnClickListener{
 		super.onActivityResult(requestCode, resultCode, data);
 	}
 
+	private Handler mHandler = new Handler(){
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			if(mPhotoList != null && mPhotoList.size() > 0){
+				mIvAddPicture.setVisibility(View.GONE);
+				mRvAddPicture.setVisibility(View.VISIBLE);
+			}else{
+				mIvAddPicture.setVisibility(View.VISIBLE);
+				mRvAddPicture.setVisibility(View.GONE);
+			}
+
+			List<MediaInfo> mediaInfos = new ArrayList<>() ;
+
+			for(int i = 0 ;i < mPhotoList.size() ;i++){
+				MediaInfo mMediaOrg = new MediaInfo();
+				mMediaOrg.setType(0);
+				mMediaOrg.setFilePath(mPhotoList.get(0).getPhotoPath());
+				mediaInfos.add(mMediaOrg) ;
+			}
+
+			initMeidaList(mediaInfos);
+		}
+	} ;
+
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -110,18 +178,13 @@ public class FeedBackActivity extends BaseActivity implements OnClickListener{
 			case R.id.ll_commit :
 				break ;
 			case R.id.iv_add_pic :
-				BJCommonImageCropHelper.openImageSingleAblum(FeedBackActivity.this, BJCommonImageCropHelper.PhotoCropType.None,
+				BJCommonImageCropHelper.openImageMulti(FeedBackActivity.this, mPhotoList,4,
 						new ThemeConfig.Builder().setMainElementsColor(Color.parseColor("#00ccff")).setTitlebarRightButtonText(R.string.complete).build(), new BJCommonImageCropHelper.OnHandlerResultCallback(){
 
 							@Override
 							public void onHandlerSuccess(List<PhotoInfo> resultList) {
-
-								MediaInfo mMediaOrg = new MediaInfo();
-								mMediaOrg.setType(0);
-								mMediaOrg.setFilePath(resultList.get(0).getPhotoPath());
-								/*initMediaDataOrg(mMediaOrg) ;
-								compressFileListOrg();*/
-
+								mPhotoList = resultList ;
+								mHandler.sendEmptyMessage(0) ;
 							}
 
 							@Override
