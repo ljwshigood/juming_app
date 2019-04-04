e5dfa870d120b535574bdbb116b7fb258a934b69
Hupo <ljwshigood@163.com>
2019-04-04 14:25:43 +0800

    fads

--- app/src/main/java/com/zzteck/jumin/ui/business/ReleaseActivity.java
+++ app/src/main/java/com/zzteck/jumin/ui/business/ReleaseActivity.java
@@ -2,11 +2,14 @@ package com.zzteck.jumin.ui.business;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.icechn.videorecorder.ui.RecordingActivity2;
 import com.zzteck.jumin.R;
 import com.zzteck.jumin.ui.mainui.BaseActivity;
 
@@ -17,12 +20,16 @@ public class ReleaseActivity extends BaseActivity implements View.OnClickListene
 	private TextView mTvMainInfo ;
 
 	private RelativeLayout mRlBack;
+
+	private LinearLayout mLLAddVideo ;
 	
 	private void initView(){
 		mTvMainInfo = findViewById(R.id.tv_main_info) ;
+		mLLAddVideo = findViewById(R.id.ll_add_video) ;
 		mRlBack = findViewById(R.id.ll_back) ;
 		mRlBack.setOnClickListener(this);
 		mRlBack.setVisibility(View.VISIBLE);
+		mLLAddVideo.setOnClickListener(this);
 	}
 	
 	private void initData(){
@@ -40,9 +47,14 @@ public class ReleaseActivity extends BaseActivity implements View.OnClickListene
 
 	@Override
 	public void onClick(View view) {
+		Intent intent = null ;
 		switch (view.getId()){
 			case R.id.ll_back:
 				finish();
+				break ;
+			case R.id.ll_add_video :
+				intent = new Intent(mContext, RecordingActivity2.class);
+				startActivity(intent);
 				break ;
 		}
 	}
