954d920d3f4e2286e91c600e913e0da8001b87bc
Hupo <ljwshigood@163.com>
2019-04-16 14:50:38 +0800

    fadsf

--- app/src/main/java/com/zzteck/jumin/ui/mainui/SearchActivity.java
+++ app/src/main/java/com/zzteck/jumin/ui/mainui/SearchActivity.java
@@ -1,13 +1,17 @@
 package com.zzteck.jumin.ui.mainui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.zzteck.jumin.R;
 import com.zzteck.jumin.adapter.SearchAdapter;
 import com.zzteck.jumin.bean.SearchBean;
+import com.zzteck.jumin.ui.business.CategoryListActivity;
 import com.zzteck.zzview.WJSpandGridView;
 
 import java.util.ArrayList;
@@ -23,13 +27,17 @@ public class SearchActivity extends BaseActivity implements View.OnClickListener
 
     private SearchAdapter mSearchAdapter ;
 
+    private TextView mTvSearch ;
+
     private void initView(){
+        mTvSearch = findViewById(R.id.iv_search) ;
         mLLBack = findViewById(R.id.ll_back) ;
         mGvSearchHint = findViewById(R.id.gv_search) ;
         mLLBack.setOnClickListener(this);
+        mTvSearch.setOnClickListener(this);
     }
 
-    private String[] searchArray = new String[]{"春秋战国","明清时期","秦汉时期","上古三代","宋元时期","隋唐五代","新石器时代"} ;
+    private String[] searchArray = new String[]{"找工作","二手房","租房","房产","司机","找房子","兼职"} ;
 
     private void initData(){
         for(int i = 0 ;i < 7 ;i++){
@@ -40,6 +48,15 @@ public class SearchActivity extends BaseActivity implements View.OnClickListener
 
         mSearchAdapter = new SearchAdapter(mContext,mSearchList) ;
         mGvSearchHint.setAdapter(mSearchAdapter);
+        mGvSearchHint.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(mContext, CategoryListActivity.class) ;
+                intent.putExtra("title","搜索") ;
+                intent.putExtra("id",1+"") ;
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -55,6 +72,12 @@ public class SearchActivity extends BaseActivity implements View.OnClickListener
         switch (v.getId()){
             case R.id.ll_back :
                 finish();
+                break ;
+            case R.id.iv_search :
+                Intent intent = new Intent(mContext, CategoryListActivity.class) ;
+                intent.putExtra("title","搜索") ;
+                intent.putExtra("id",1+"") ;
+                startActivity(intent);
                 break ;
         }
     }
