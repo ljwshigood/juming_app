3cea64a3b774b693ee679115d8726e229411af6c
ljwshigood <ljwshigood163.com>
2019-05-20 17:42:21 +0800

    fasdf

--- app/src/main/java/com/zzteck/jumin/ui/business/ReleaseActivity.java
+++ app/src/main/java/com/zzteck/jumin/ui/business/ReleaseActivity.java
@@ -42,6 +42,8 @@ import com.luck.picture.lib.config.PictureMimeType;
 import com.luck.picture.lib.entity.LocalMedia;
 import com.zzteck.jumin.R;
 import com.zzteck.jumin.adapter.CheckAdapter;
+import com.zzteck.jumin.adapter.FullyGridLayoutManager;
+import com.zzteck.jumin.adapter.GridImageAdapter;
 import com.zzteck.jumin.adapter.ImageAdapter;
 import com.zzteck.jumin.adapter.QoneAdapter;
 import com.zzteck.jumin.adapter.StreamAdapter;
@@ -131,6 +133,69 @@ public class ReleaseActivity extends BaseActivity implements View.OnClickListene
 
 	private RecyclerView mRvPic ;
 
+	private GridImageAdapter adapter ;
+
+
+	private GridImageAdapter.onAddPicClickListener onAddPicClickListener = new GridImageAdapter.onAddPicClickListener() {
+		@Override
+		public void onAddPicClick() {
+
+			PictureSelector.create(ReleaseActivity.this)
+					.openGallery(PictureMimeType.ofAll())// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+					.theme(R.style.picture_default_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
+					.maxSelectNum(4)// 最大图片选择数量
+					.minSelectNum(1)// 最小选择数量
+					.imageSpanCount(4)// 每行显示个数
+					.selectionMode(PictureConfig.MULTIPLE)// 多选 or 单选
+					.previewImage(true)// 是否可预览图片
+					.previewVideo(true)// 是否可预览视频
+					.enablePreviewAudio(true) // 是否可播放音频
+					.isCamera(true)// 是否显示拍照按钮
+					.isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+					//.imageFormat(PictureMimeType.PNG)// 拍照保存图片格式后缀,默认jpeg
+					//.setOutputCameraPath("/CustomPath")// 自定义拍照保存路径
+					.enableCrop(true)// 是否裁剪
+					.compress(true)// 是否压缩
+					.synOrAsy(true)//同步true或异步false 压缩 默认同步
+					//.compressSavePath(getPath())//压缩图片保存地址
+					//.sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+					.glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+					.withAspectRatio(3, 4)// 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
+					.hideBottomControls(true)// 是否显示uCrop工具栏，默认不显示
+					.isGif(false)// 是否显示gif图片
+					.freeStyleCropEnabled(true)// 裁剪框是否可拖拽
+					.circleDimmedLayer(false)// 是否圆形裁剪
+					.showCropFrame(false)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
+					.showCropGrid(false)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
+					.openClickSound(false)// 是否开启点击声音
+					.selectionMedia(selectList)// 是否传入已选图片
+					//.isDragFrame(false)// 是否可拖动裁剪框(固定)
+//                        .videoMaxSecond(15)
+//                        .videoMinSecond(10)
+					//.previewEggs(false)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中)
+					//.cropCompressQuality(90)// 裁剪压缩质量 默认100
+					.minimumCompressSize(100)// 小于100kb的图片不压缩
+					//.cropWH()// 裁剪宽高比，设置如果大于图片本身宽高则无效
+					//.rotateEnabled(true) // 裁剪是否可旋转图片
+					//.scaleEnabled(true)// 裁剪是否可放大缩小图片
+					//.videoQuality()// 视频录制质量 0 or 1
+					//.videoSecond()//显示多少秒以内的视频or音频也可适用
+					//.recordVideoSecond()//录制视频秒数 默认60s
+					.forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+
+		}
+
+	};
+
+	private void initReleasePic(){
+		FullyGridLayoutManager manager = new FullyGridLayoutManager(ReleaseActivity.this, 4, GridLayoutManager.VERTICAL, false);
+		mRvPic.setLayoutManager(manager);
+		adapter = new GridImageAdapter(ReleaseActivity.this, onAddPicClickListener);
+		adapter.setList(selectList);
+		adapter.setSelectMax(6);
+		mRvPic.setAdapter(adapter);
+	}
+
 	private void initView(){
 		mRvPic = findViewById(R.id.rv_pic) ;
 		mIvAddPicture = findViewById(R.id.iv_add_pic) ;
@@ -157,9 +222,9 @@ public class ReleaseActivity extends BaseActivity implements View.OnClickListene
 
 		mImageAdapter = new ImageAdapter(mContext,null) ;
 
-		GridLayoutManager linearLayoutManager1 = new GridLayoutManager(this,4);
+		/*GridLayoutManager linearLayoutManager1 = new GridLayoutManager(this,4);
 		mRvPic.setLayoutManager(linearLayoutManager1);
-		mRvPic.setAdapter(mImageAdapter);
+		mRvPic.setAdapter(mImageAdapter);*/
 	}
 
 	private HashMap mHashExtra = new HashMap() ;
@@ -1045,7 +1110,7 @@ public class ReleaseActivity extends BaseActivity implements View.OnClickListene
 
 				PictureSelector.create(ReleaseActivity.this)
 						.openGallery(PictureMimeType.ofAll())// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
-						.theme(R.style.picture_white_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
+						.theme(R.style.picture_default_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
 						.maxSelectNum(4)// 最大图片选择数量
 						.minSelectNum(1)// 最小选择数量
 						.imageSpanCount(4)// 每行显示个数
